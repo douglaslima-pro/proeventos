db85de08571ee2e1d5009dca08bc44ff0d4c2e2b
douglaslima-pro <douglaslima-pro@outlook.com>
2024-10-12 12:49:48 -0300

    feat: adiciona a classe DbContext, os arquivos de migration, as dependências do EntityFrameworkCore e os altera endpoints para recuperar os dados do arquivo com extensão "db".

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using ProEventos.API.Models;
+using ProEventos.API.Data;
 
 namespace ProEventos.API.Controllers
 {
@@ -15,45 +16,23 @@ namespace ProEventos.API.Controllers
     public class EventoController : ControllerBase
     {
 
-        public IEnumerable<Evento> eventos = new List<Evento>()
-        {
-            new Evento()
-            {
-                EventoId = 1,
-                Local = "Belo Horizonte, MG",
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                Tema = "Angular e .NET 5",
-                QtdPessoas = 250,
-                Lote = "1° Lote",
-                ImagemURL = "evento1.png"
-            },
-            new Evento()
-            {
-                EventoId = 2,
-                Local = "São Paulo, SP",
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                Tema = "Angular e Suas Novidades",
-                QtdPessoas = 350,
-                Lote = "1° Lote",
-                ImagemURL = "evento2.png"
-            }
-        };
+        public ApplicationContext context;
 
-        public EventoController()
+        public EventoController(ApplicationContext context)
         {
-
+            this.context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return eventos;
+            return context.Eventos;
         }
 
         [HttpGet("{id}")]
         public Evento Get(int id)
         {
-            return eventos.Where(evento => evento.EventoId == id).FirstOrDefault();
+            return context.Eventos.Where(evento => evento.EventoId == id).FirstOrDefault();
         }
 
     }
